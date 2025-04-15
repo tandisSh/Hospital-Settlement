1fad4fbfeeaf89ea749a1f18c6bba659b397165e
tandisSh <tandis00shojaee@gmail.com>
2025-04-15 15:30:41 +0330

    doctor invoicesList api

---
+++ app/Http/Controllers/api/doctor/invoiceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\api\doctor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\InvoiceResource;
+use App\Models\Invoice;
+
+class invoiceController extends Controller
+{
+    public function index(Request $request)
+    {
+        $doctor = $request->user();
+
+        $invoices = Invoice::where('doctor_id', $doctor->id)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return InvoiceResource::collection($invoices);
+    }
+}
+
+
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\doctor\AuthController;
+use App\Http\Controllers\api\doctor\invoiceController;
 use App\Http\Controllers\api\doctor\SurgeryController;
 
 Route::prefix('doctor')->group(function () {
@@ -14,5 +15,6 @@ Route::prefix('doctor')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/surgeries/list', [SurgeryController::class, 'list']);
         Route::get('/surgery/{id}', [SurgeryController::class, 'show']);
+        Route::get('/invoices', [invoiceController::class, 'index']);
     });
 });
